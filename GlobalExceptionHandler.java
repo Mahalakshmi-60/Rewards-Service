12d6eda5d213bc0a292ebd4d0355cdc0a0cbb138
Mahalakshmi-60 <mahalakshmi.b.b@capgemini.com>
2025-05-27 11:33:38 +0530

    Update GlobalExceptionHandler.java

--- GlobalExceptionHandler.java
+++ GlobalExceptionHandler.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.*;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+@ExceptionHandler(RewardsCalculationException.class)
+public ResponseEntity<String> handleRewardsException(RewardsCalculationException ex)
+{
+return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+}
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAll(Exception ex) {
         return new ResponseEntity<>("Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
